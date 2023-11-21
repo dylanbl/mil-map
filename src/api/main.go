3f75a0a7d79b0afdeab82f315b505a9a724dd79e
Dylan Lewis <dbl27@proton.me>
2023-11-21 00:02:56 -0500

    added new basemap

---
+++ src/api/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func main() {
+	dbpool, err := pgxpool.New(context.Background(), "postgres://postgres:postgres@localhost:5432/world-stats")
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
+		os.Exit(1)
+	}
+
+	defer dbpool.Close()
+}
